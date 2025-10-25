92131546b1d69dfcb592546534f130fa84837406
Andrey20001 <162813529+Andrey20001@users.noreply.github.com>
2025-10-25 18:14:47 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,47 @@
+import telebot
+from bot_logic import gen_pass, gen_emodji, flip_coin
+
+
+bot = telebot.TeleBot("")
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Привет! Я Змейка!")
+
+
+@bot.message_handler(commands=['Привет!'])
+def send_hello(message):
+    bot.send_message(message.chat.id, "Привет! Как дела?")
+
+
+@bot.message_handler(commands=['Пока!'])
+def send_bye(message):
+    bot.send_message(message.chat.id, "Пока! Удачи!")
+
+@bot.message_handler(commands=['password'])
+def send_password(message):
+    bot.reply_to(message, gen_pass(12))
+
+@bot.message_handler(commands=['emodji'])
+def send_emodji(message):
+    emodji = gen_emodji()
+    bot.reply_to(message, "Вот эмоджи: " + emodji)
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, "Монетка выпала так: " + coin)
+
+
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if message.text == "Ты тупой":
+        bot.send_message(message.chat.id, "Сам тупой")
+    else:
+        bot.send_message(message.chat.id, "Чё надо?")
+
+
+bot.polling()
